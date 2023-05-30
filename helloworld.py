7ed06e08063a5662113975022eddf36f4a32723b
AzureAD\PhillipRudolph <phillip.rudolph@skaylink.com>
2023-05-30 10:52:44 +0200

    basic input

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Hello I am a Chatbot. Type 'exit' to end program")
+
+while True:
+    anfrage = input(":")
+    if anfrage == "exit":
+        break
+    else:
+        print("echo: " + anfrage)
+
