65b6371598168f0619e2381013ab314780b3d844
AzureAD\PhillipRudolph <phillip.rudolph@skaylink.com>
2023-05-31 09:29:56 +0200

    rollback hello world

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,1 @@
-print("Hello I am a Chatbot. Type 'exit' to end program")
-
-while True:
-    anfrage = input(":")
-    if anfrage == "exit":
-        break
-    else:
-        print("echo: " + anfrage)
-
+print("Hello World")
