26a90629d4d0a30c3b309e5b109bf0cb5e6d083c
AzureAD\PhillipRudolph <phillip.rudolph@skaylink.com>
2023-05-31 12:26:25 +0200

    better schleife

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 print("Hello I am a Chatbot. Type 'exit' to end program")
 anfrage = input("")
-while anfrage != "exit":                                                         #schleife
+while anfrage != "exit":
     anfrage = input("")
     print("echo: " + anfrage)
