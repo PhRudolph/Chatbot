c36f8cd254bb33d9e21c41730294d4d9dac07e07
Luca Kamrath <luca.kmrth@gmail.com>
2023-06-07 11:02:39 +0200

    Added mail service with unit tests

--- module/mail_service/test_mail_service.py
+++ module/mail_service/test_mail_service.py
@@ -1,52 +1,32 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
+from mail_service import mail_service
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import smtplib
-
-from mail_service import mail_service
 
 class MailServiceTests(unittest.TestCase):
 
-    @patch('smtplib.SMTP')
-    def test_mail_service(self, mock_smtp):
-        subject = "This is the subject."
-        body = "This is the email message."
+    def test_send_email(self):
+        with patch('smtplib.SMTP') as mock_smtp:
+            mail_service.smtp_server = "test_server"
+            mail_service.smtp_port = 1234
+            mail_service.from_address = "test@example.com"
+            mail_service.send_email(MIMEMultipart())
 
-        mock_smtp_instance = MagicMock(spec=mail_service)
-        mock_smtp_instance.starttls = MagicMock(spec=mail_service)
-        mock_smtp_instance.login = MagicMock(spec=mail_service)
-        mock_smtp_instance.send_message = MagicMock()
+            mock_smtp.assert_called_once_with("test_server", 1234)
 
-        # Assert that SMTP was instantiated with the correct parameters
-        mock_smtp.assert_called_once_with("smtp.gmail.com", 587)
+    @patch('builtins.print')
+    @patch('mail_service.mail_service.send_email')
+    def test_mail_service(self, mock_send_email, mock_print):
+        mail_service.from_address = "from@example.com"
+        mail_service.to_address = "to@example.com"
+        subject = "Test Subject"
+        body = "Test Body"
 
-        # Assert that starttls() was called
-        mock_smtp_instance.starttls.assert_called_once()
+        mail_service.mail_service(subject, body)
 
-        # Assert that login() was called with the correct parameters
-        mock_smtp_instance.login.assert_called_once_with("luca.kmrth@gmail.com", 'krmrkyvnkqtfrpbg')
-
-        # Assert that send_message() was called with the correct parameters
-        expected_message = MIMEMultipart()
-        expected_message['From'] = "luca.kmrth@gmail.com"
-        expected_message['To'] = "lucakamrath.arbeit@gmail.com"
-        expected_message['Subject'] = subject
-        expected_message.attach(MIMEText(body, 'plain'))
-        mock_smtp_instance.send_message.assert_called_once_with(expected_message)
-
-    @patch('smtplib.SMTP')
-    def test_mail_service_exception(self, mock_smtp):
-        subject = "This is the subject."
-        body = "This is the email message."
-
-        # Simulate an exception during the email sending process
-        mock_smtp_instance = mock_smtp.return_value
-        mock_smtp_instance.__enter__.side_effect = Exception("An error occurred")
-
-        # Assert that an exception is raised
-        with self.assertRaises(Exception):
-            mail_service(subject, body)
+        mock_send_email.assert_called_once()
+        mock_print.assert_called_once_with('Email sent successfully!')
 
 if __name__ == '__main__':
     unittest.main()
